a9ecadea0bc0e8dd36d9701e58b72e93b61c1ae7
Your Name <you@example.com>
2018-12-14 19:39:55 +0400

    goxaca

---
+++ grass_class.js
@@ -0,0 +1,15 @@
+var LivingCreature = require("./class.js")
+module.exports=class Grass extends LivingCreature {
+
+    mult() {
+        var empty = random(this.chooseCell(0))
+        this.multiply++
+        if (empty && this.multiply < 4) {
+            var newX = empty[0]
+            var newY = empty[1]
+            matrix[newY][newX] = 1
+            var gr = new Grass(newX, newY, 1)
+            grassArr.push(gr)
+        }
+    }
+}
